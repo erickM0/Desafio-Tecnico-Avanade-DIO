b85460bb6fff11c67e6c37afab159a2cadefa78c
erickM0 <erickmacedo12@gmail.com>
2025-09-04 00:03:29 -0300

    Including DBContext file

---
+++ StockService/StockService.Infra/DB/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace StockService.Infra;
+
+public class DBContext : DbContext
+{
+    private readonly IConfiguration _configurationAppSettings;
+    public DBContext(IConfiguration configurationAppSettings)
+    {
+        _configurationAppSettings = configurationAppSettings;
+    }
+}
