ab6b5cbdc20402dece044840e1437d80fd6dbf55
erickM0 <erickmacedo12@gmail.com>
2025-09-04 00:02:07 -0300

    Adding Product Entity

---
+++ StockService/StockService.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockService.Domain;
+
+public class Product
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; } = default;
+    public required string Name { get; set; }
+    private bool Active { get; set; } = true;
+
+    private DateTime CratedAt { get; set; }
+    private DateTime LastModifiedAt { get; set; }
+    
+
+ 
+}
