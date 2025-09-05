c252a2852160ebc25921faece8793177639a5ac3
erickM0 <erickmacedo12@gmail.com>
2025-09-05 18:08:42 -0300

    Removing white spaces

--- StockService/StockService.Domain/Entities/Product.cs
+++ StockService/StockService.Domain/Entities/Product.cs
@@ -16,15 +16,12 @@ public class Product : IAuditableModified
     [Required]
     [StringLength(200)]
     public required string Name { get; set; }
-    public DateTime CreatedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     [Required]
     [Column(TypeName = "bit")]
     private bool Active { get; set; } = true;
-
     [Required]
     [Column(TypeName = "datetime2")]
     public DateTime CratedAt { get; set; }
-
     [Required]
     [Column(TypeName = "datetime2")]
     public DateTime LastModifiedAt { get; set; }
