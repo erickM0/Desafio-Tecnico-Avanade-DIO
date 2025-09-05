041d8bfa9f80e3fbb0eea7be89d64bf4c94c5ee9
erickM0 <erickmacedo12@gmail.com>
2025-09-04 21:47:43 -0300

    Include StockMovement Entity

---
+++ StockService/StockService.Domain/Entities/StockMovement.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using StockService.Domain.Interfaces;
+using StockService.Domain.Enums;
+
+namespace StockService.Domain.Entities;
+
+public class StockMovement : IAuditable
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    public int ProductId { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; }
+    [Required]
+    public MovementTypes MovementType { get; set; }
+    public string Observations { get; set; } 
+}
