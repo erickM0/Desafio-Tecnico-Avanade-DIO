e594f758e54a18c5e90c70a5638cfc04c0238108
erickM0 <erickmacedo12@gmail.com>
2025-09-04 23:31:20 -0300

    StockService.Domain.DTO

---
+++ StockService/StockService.Domain/DTO/ProductDto.cs
@@ -0,0 +1,10 @@
+
+namespace StockService.Domain.DTO;
+
+public class ProductDto
+{
+    public string SKU { get; set; }
+    public required string Name { get; set; }
+    
+    
+}
---
+++ StockService/StockService.Domain/DTO/StockMovementDto.cs
@@ -0,0 +1,7 @@
+namespace StockService.Domain.DTO;
+
+public class StockMovementDto
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+}
