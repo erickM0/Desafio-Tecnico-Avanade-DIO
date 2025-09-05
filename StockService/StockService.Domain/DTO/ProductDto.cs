d760292e6dc92806a8d5a0b5448b427f43bcce1e
erickM0 <erickmacedo12@gmail.com>
2025-09-05 18:09:58 -0300

    adding properties to ProductDTO

--- StockService/StockService.Domain/DTO/ProductDto.cs
+++ StockService/StockService.Domain/DTO/ProductDto.cs
@@ -3,8 +3,9 @@ namespace StockService.Domain.DTO;
 
 public class ProductDto
 {
-    public string SKU { get; set; }
+    public required string SKU { get; set; }
     public required string Name { get; set; }
-    
-    
+    public required bool Active { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastModifiedAt { get; set; }
 }
