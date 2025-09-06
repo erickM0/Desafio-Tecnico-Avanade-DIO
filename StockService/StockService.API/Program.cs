cd10eb630cf09f29a35e4bbc0def9508388ca660
erickM0 <erickmacedo12@gmail.com>
2025-09-06 02:47:47 -0300

    implementing builder.services (services, repo and interface)

--- StockService/StockService.API/Program.cs
+++ StockService/StockService.API/Program.cs
@@ -1,13 +1,22 @@
+using StockService.Infra.DB;
+using StockService.Domain.Services;
+using StockService.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using StockService.Infra.DB.Repositories;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("SqlServer");
-
 
 
 builder.Services.AddDbContext<StockDbContext>(options =>
     options.UseSqlServer(connectionString)
 );
 
+builder.Services.AddScoped<IProductDataAccess, ProductRepository>();
+
+builder.Services.AddScoped<ProductServices>();
 
 var app = builder.Build();
 
