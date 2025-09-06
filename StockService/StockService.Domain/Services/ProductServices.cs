6da29979a477702b44fb52acd9662b5c029a4952
erickM0 <erickmacedo12@gmail.com>
2025-09-06 02:43:42 -0300

    sku fixed fom  int to string

--- StockService/StockService.Domain/Services/ProductServices.cs
+++ StockService/StockService.Domain/Services/ProductServices.cs
@@ -1,34 +1,24 @@
+using StockService.Domain.Entities;
+using StockService.Domain.Interfaces;
+
 namespace StockService.Domain.Services;
 
 public class ProductServices
 {
-    private readonly  IProductDataAccess _productDataAccess;
 
+    private readonly IProductDataAccess _dataAccess;
 
     public ProductServices(IProductDataAccess productDataAccess)
     {
-        _productDataAccess = productDataAccess;
+        _dataAccess = productDataAccess;
     }
 
-    public  async Task<List<Product>> ListProducts(int? page)
-    {
-        var query = _productsDbContext.Products.AsQueryable();
-        int pageInt = null ? (int)page : 1;
-        int itemsPerPage = 100;
-
-        query = query
-                .Skip((pageInt - 1) * itemsPerPage)
-                .Take(itemsPerPage);
-
-        return await query.ToListAsync();
-
+    public async Task<Product?> GetBySku(string sku){
+        return await _dataAccess.GetBySku(sku);
     }
 
-    public async Task<bool> Create(Product product)
-    {
-        _productsDbContext.Product.AddAsync(product);
+    
 
-        return await _productsDbContext.SaveChangesAsync() > 0;
-    }
+    
 
 }
