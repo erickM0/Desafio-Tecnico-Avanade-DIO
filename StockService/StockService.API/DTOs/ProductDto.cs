bf4c7f4503716e498a1b7c5001cd51a946c8ceb3
erickM0 <erickmacedo12@gmail.com>
2025-09-10 17:31:53 -0300

    add dtos

---
+++ StockService/StockService.API/DTOs/ProductDto.cs
@@ -0,0 +1,9 @@
+namespace StockService.API.DTOs;
+
+public class ProductDto
+{
+    public required string Sku { get; set; }
+    public required string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; } = default;
+}
